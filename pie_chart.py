a5eccda7c14cc92881f711a1df63407cfd4eb772
Ed <edsagoefvr@gmail.com>
2023-09-05 08:38:28 +0000

    Made pie chart

---
+++ pie_chart.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+cars = pd.read_csv('vw.csv')
+
+
+#Pie chart  showing distribution b/n fuel types
+number_of_cars_by_fuel = cars.groupby('fuelType')[['model']].count().sort_values('model', ascending=False).reset_index()
+
+plt.pie(number_of_cars_by_fuel.model, labels=number_of_cars_by_fuel.fuelType, autopct='%1.1f%%')
+plt.show()
